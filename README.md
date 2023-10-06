1532f6ebe14f697ec829632239a2f8f6ffe0a754
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-10-05 22:38:15 -0300

    update
    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # django-project-02
  project v2
+
+# DRF
+> 
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,48 +1,48 @@
 from django.urls import path
 
-from . import views
+from .views import site
 
 app_name = 'recipes'
 
 urlpatterns = [
     path(
         '',
-        views.RecipeListViewHome.as_view(),
+        site.RecipeListViewHome.as_view(),
         name="home"
     ),
     path(
         'recipes/search/',
-        views.RecipeListViewSearch.as_view(),
+        site.RecipeListViewSearch.as_view(),
         name="search"
     ),
     path(
         'recipes/tags/<slug:slug>/',
-        views.RecipeListViewTag.as_view(),
+        site.RecipeListViewTag.as_view(),
         name="tag"
     ),
     path(
         'recipes/category/<int:category_id>/',
-        views.RecipeListViewCategory.as_view(),
+        site.RecipeListViewCategory.as_view(),
         name="category"
     ),
     path(
         'recipes/<int:pk>/',
-        views.RecipeDetail.as_view(),
+        site.RecipeDetail.as_view(),
         name="recipe"
     ),
     path(
         'recipes/api/v1/',
-        views.RecipeListViewHomeApi.as_view(),
+        site.RecipeListViewHomeApi.as_view(),
         name="recipes_api_v1",
     ),
     path(
         'recipes/api/v1/<int:pk>/',
-        views.RecipeDetailAPI.as_view(),
+        site.RecipeDetailAPI.as_view(),
         name="recipes_api_v1_detail",
     ),
     path(
         'recipes/theory/',
-        views.theory,
+        site.theory,
         name='theory',
     )
 ]
